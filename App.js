8ad913d90ac05f05c7e029509c57dac032e7f6bc
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-05 19:25:24 -0600

    first core component View

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View,Text } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "plum"
+    }} >
+    <View 
+    style={{
+      width:250,
+      height:200,
+      backgroundColor: "lightblue"
+    }}
+    ></View>
+     <View 
+    style={{
+      width:200,
+      height:200,
+      backgroundColor: "lightgreen"
+    }}
+    ></View>
+  </View>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
