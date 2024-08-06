e26378dbe870c3d2669b78c7e96930cf83b7f1ef
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-05 22:06:17 -0600

    workin with Pressable

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import { View, Text, Image, ScrollView, Button } from "react-native";
+import { View, Text, Image, ScrollView, Button, Pressable } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 const logoImg = require("./assets/adaptive-icon.png")
 
 export default function App() {
   return <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }} >
     <Button
-    title="Press"
-    onPress={()=>console.log("you have press the btn")}
-    color="midnightblue"
-    disabled={false}
+      title="Press"
+      onPress={() => console.log("you have press the btn")}
+      color="midnightblue"
+      disabled={false}
     />
+    <ScrollView >
+      <Pressable
+      onPress={()=> console.log("image press")}
+      >
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Text>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    </ScrollView>
   </View>
 }
 
-{/*disabled: for default is True  */}
+{/*disabled: for default is True  */ }
