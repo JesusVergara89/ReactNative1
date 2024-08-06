90579c31e38cd1cd97b73db6493e5dee21a3bb32
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-05 21:20:37 -0600

    workin with scrollview

--- App.js
+++ App.js
@@ -1,51 +1,17 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png")
-const BackImg = require("./assets/favicon.png")
 
 export default function App() {
-  return <View style={{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "plum"
-  }} >
-    <View
-      style={{
-        width: 250,
-        height: 200,
-        backgroundColor: "lightblue"
-      }}
-    >
-      <Text
-        style={{
-          textAlign: "center",
-          paddingTop: "50%"
-        }}
-      >
-        <Image
-          style={{ width: 50, height: 50 }}
-          source={{ uri: "https://picsum.photos/50" }}
-        />
-        <Text style={{ color: "red" }}>Hello</Text> word
+  return <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }} >
+    <ScrollView >
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </Text>
-    </View>
-    <View
-      style={{
-        width: 200,
-        height: 200,
-        backgroundColor: "lightgreen"
-      }}
-    >
-      <Image
-        style={{ width: "contain", height: "100%" }}
-        source={logoImg}
-      />
-    </View>
-    <ImageBackground
-    style={{flex: 1}}
-     source={BackImg}>
-           <Text>IMAGE TEXT</Text>
-    </ImageBackground>
-
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    </ScrollView>
   </View>
 }
