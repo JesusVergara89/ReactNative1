6a30f376f998f09ab0dbc9214342492987f44f05
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-05 20:02:24 -0600

    first core component Image and ImageBackGround

--- App.js
+++ App.js
@@ -1,34 +1,51 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png")
+const BackImg = require("./assets/favicon.png")
 
 export default function App() {
   return <View style={{
-    flex:1,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "plum"
-    }} >
-    <View 
-    style={{
-      width:250,
-      height:200,
-      backgroundColor: "lightblue"
-    }}
+  }} >
+    <View
+      style={{
+        width: 250,
+        height: 200,
+        backgroundColor: "lightblue"
+      }}
     >
-     <Text 
-     style={{
-      textAlign: "center",
-      paddingTop: "50%"
-     }}
-     >
-      <Text style={{color: "red"}}>Hello</Text> word
+      <Text
+        style={{
+          textAlign: "center",
+          paddingTop: "50%"
+        }}
+      >
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={{ uri: "https://picsum.photos/50" }}
+        />
+        <Text style={{ color: "red" }}>Hello</Text> word
       </Text>
     </View>
-     <View 
-    style={{
-      width:200,
-      height:200,
-      backgroundColor: "lightgreen"
-    }}
-    ></View>
+    <View
+      style={{
+        width: 200,
+        height: 200,
+        backgroundColor: "lightgreen"
+      }}
+    >
+      <Image
+        style={{ width: "contain", height: "100%" }}
+        source={logoImg}
+      />
+    </View>
+    <ImageBackground
+    style={{flex: 1}}
+     source={BackImg}>
+           <Text>IMAGE TEXT</Text>
+    </ImageBackground>
+
   </View>
 }
