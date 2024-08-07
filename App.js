fd83592dd7da65fcafe34c0bb901ae9f1285cf90
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-07 15:00:01 -0600

    reusable components

--- App.js
+++ App.js
@@ -1,33 +1,20 @@
 import { useState } from "react";
 import { View, Text, Image, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
+import Greet from "./components/Greet";
 //const logoImg = require("./assets/adaptive-icon.png")
 
 
-
 export default function App() {
-  const [hideStatusBar, setHideStatusBar] = useState(false);
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white', padding: 60 }}>
-     <Button title="Alert"
-     onPress={()=> Alert.alert("invalid data!")}
-     />
-      <Button title="Alert2"
-     onPress={()=> Alert.alert("invalid data!", "DOB incorrect")}
-     />
-     <Button title="Alert3"
-     onPress={()=> Alert.alert("invalid data!", "DOB incorrect", [
-      {
-        text: "Cancel",
-        onPress: () => console.log("cancel press")
-      },
-      {
-        text: "Ok",
-        onPress: () => console.log("ok press")
-      }
-     ])}
-     />
+    <Greet name="Jesus" />
+    <Greet name="Rogert" />
+    <Greet name="Samuel" />
+    <Greet name="Edilma" />
+    <Greet name="Renata" />
+    <Greet name="Romina" />
     </View>
   );
 }
