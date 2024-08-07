b688d9d58265f5638f566f8ab6e445cdad49a0f7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-07 13:56:59 -0600

    Alert

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, Image, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator } from "react-native";
+import { View, Text, Image, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 //const logoImg = require("./assets/adaptive-icon.png")
 
@@ -10,20 +10,24 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, backgroundColor: 'white', padding: 60 }}>
-      <ActivityIndicator
-      />
-      <ActivityIndicator
-      size="large"
-      />
-      <ActivityIndicator
-      color="midnightblue"
-      size="large"
-      />
-      <ActivityIndicator
-      color="midnightblue"
-      size="large"
-      animating={true}
-      />
+     <Button title="Alert"
+     onPress={()=> Alert.alert("invalid data!")}
+     />
+      <Button title="Alert2"
+     onPress={()=> Alert.alert("invalid data!", "DOB incorrect")}
+     />
+     <Button title="Alert3"
+     onPress={()=> Alert.alert("invalid data!", "DOB incorrect", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("cancel press")
+      },
+      {
+        text: "Ok",
+        onPress: () => console.log("ok press")
+      }
+     ])}
+     />
     </View>
   );
 }
