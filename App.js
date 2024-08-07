c36b4959b003c3edd576a4971dbad0d3fb64021f
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-06 23:55:40 -0600

    model component and its props, like visible,onRequestClose,animationType,presentationStyle

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
-import { View, Text, Image, ScrollView, Button, Pressable } from "react-native";
+import { useState } from "react";
+import { View, Text, Image, ScrollView, Button, Pressable, Modal } from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
-const logoImg = require("./assets/adaptive-icon.png")
+//const logoImg = require("./assets/adaptive-icon.png")
+
+
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false)
   return <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }} >
+    
     <Button
-      title="Press"
-      onPress={() => console.log("you have press the btn")}
-      color="midnightblue"
-      disabled={false}
+    title="View modal"
+    onPress={() => setIsModalVisible(true)}
+    color="midnightblue"
     />
-    <ScrollView >
-      <Pressable
-      onPress={()=> console.log("image press")}
-      >
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </Pressable>
-      <Text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </Text>
-      <Image source={logoImg} style={{ width: 300, height: 300 }} />
-    </ScrollView>
+
+    <Modal visible={isModalVisible} onRequestClose={()=>setIsModalVisible(false)} animationType="slice" presentationStyle="pageSheet">
+      <View style={{flex:1, backgroundColor: "lightgreen", padding: 60}}>
+           <Text>Modal content</Text>
+           <Button
+           title="Close"
+           onPress={() => setIsModalVisible(false)}
+           color="midnightblue"
+           />
+      </View>      
+    </Modal>
   </View>
 }
 
